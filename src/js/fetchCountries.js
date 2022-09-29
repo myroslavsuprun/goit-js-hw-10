2e26bda0e1946614e930e05427a36bee1bda0f16
MiroProger <sirmiroslavsuprun@gmail.com>
2022-09-29 16:06:46 +0300

    Corrected mistakes in countries fetch

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,7 +2,12 @@ const BASE_URL = 'https://restcountries.com/v3.1/name/';
 
 export function fetchCountries(name) {
   const urlToFetch = `${BASE_URL}${name}?fields=capital,population,languages,flags,name`;
-  return fetch(urlToFetch).then(response => response.json());
+  return fetch(urlToFetch)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .catch(() => Notify.failure('Oops, there is no country with such name'));
 }
-
-// Нужно как-то исправить на свг и на name.official
